6e0919ff7ee86c4fba5c6ec520a14f635bd0b895
econraddiao <econraddiao@gmail.com>
2020-07-14 15:05:20 -0700

    Initial Commit

---
+++ swift-calculator-tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-calculator-tutorial
+//
+//  Created by Conrad Diao on 7/14/20.
+//  Copyright © 2020 E. Conrad Diao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
